f5d2bc1adcd084567996db081ed1e05ed331f8e9
Andy <zhengxinyao888@gmail.com>
2021-09-12 21:42:29 -0400

    Initial Commit

---
+++ flix project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flix project
+//
+//  Created by Andy Zheng on 9/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
