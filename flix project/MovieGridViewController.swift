d30282a8350d7437b7fabd1898e01bb75802888c
Andy <zhengxinyao888@gmail.com>
2021-09-19 21:30:38 -0400

    configured collection view layout

--- flix project/MovieGridViewController.swift
+++ flix project/MovieGridViewController.swift
@@ -19,6 +19,18 @@ class MovieGridViewController: UIViewController,UICollectionViewDataSource,UICol
         
         collectionView.delegate = self
         collectionView.dataSource = self
+        
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumInteritemSpacing * 2) / 3
+        //Width changes depending on the device
+        
+        layout.itemSize = CGSize(width: width, height: width * 3 / 2)
+        
+        
 
         let url = URL(string:
             "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
