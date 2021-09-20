a8cf727f6e4cc28b7ddf4844f006b82e53da036a
Andy <zhengxinyao888@gmail.com>
2021-09-19 20:00:20 -0400

    super heros collection view finished

---
+++ flix project/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  MovieGridCell
+//
+//  Created by Andy Zheng on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
